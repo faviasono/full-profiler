05e12ec8226a84db3dd9fb34e8913231a948f33f
Andrea Favia <andrea.faviait@gmail.com>
2022-11-23 11:40:57 +0000

    Add typer.abort to handle exceptions

--- full_profiler/main.py
+++ full_profiler/main.py
@@ -49,9 +49,10 @@ def time_memory(path: str):
     Use memory_profiler to generate statistics and plot results in file_name.png file
     """
     if not os.path.exists(path):
-        raise ValueError
+        raise typer.Abort()
     if not path.endswith('.py'):
-        raise ValueError('It must be a Python scipt')
+        print('It must be a Python scipt')
+        typer.Abort()
     try:
         with open(create_output_file(path), 'w') as f:
             subprocess.call(MPROF_RUN_COMMAND.format(path).split(), stdout=f)
@@ -60,6 +61,7 @@ def time_memory(path: str):
             subprocess.call(MPROF_PLOT_COMMAND.format(title, os_put).split(), stdout=f)
     except OSError as e:
         print('Error' + str(e))
+        typer.Abort()
 
 @app.command()
 def cpu_time(path: str, plot: bool = False):
@@ -67,16 +69,17 @@ def cpu_time(path: str, plot: bool = False):
     Use cProfile to generate statistics program.prof and plot results using snakeviz in browser
     """
     if not os.path.exists(path):
-        raise ValueError
+        raise typer.Abort()
     if not path.endswith('.py'):
-        raise ValueError('It must be a Python scipt')
+        raise  typer.Abort()
+       
     try:
         with open(create_output_file(path), 'w') as f:
             subprocess.call(CPROFILE_COMMAND.format(path).split(), stdout= f)
             if plot:
                 subprocess.call(SNAKEVIZ_COMMAND.split(), stdout= f)
     except OSError as e:
-        print('Error' + str(e))
+        typer.Abort()
 
 
 
